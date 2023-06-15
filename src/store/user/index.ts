9a7b755e2fd1db2dbadf83423cd473038a14c1cc
Vinicius Roza Barros <rozaviniciusdeveloper@gmail.com>
2023-06-14 22:03:24 -0300

    feat: add userStore

--- src/store/user/index.ts
+++ src/store/user/index.ts
@@ -1,1 +1,28 @@
-export {}
+import { create } from "zustand"
+
+interface UserProps{
+    id: string;
+    firstName: string;
+    lastName: string;
+}
+interface ActionsProps{
+    updateUser: (user: UserProps) => void;
+}
+
+interface StoreProps{
+    state: UserProps;
+    actions: ActionsProps;
+}
+
+const useUserStore = create<StoreProps>()((set) => ({
+    state: {
+        id: "c33633bd-64ba-4b81-9e38-52e553de805a",
+        firstName: "Vinicius",
+        lastName: "Roza Barros",
+    },
+    actions: {
+        updateUser: (user) => set(({ state }) => ({ state: {...state, ...user} })),
+    }
+}))
+
+export { useUserStore }
